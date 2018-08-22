887013c86c62202697f37cdb25d1384a1dcfdb4e
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-08-22 09:41:34 +0200

    Adding my emacs config file

---
+++ emacs.d/init.el
@@ -0,0 +1,47 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(let ((default-directory  "~/.emacs.d/lisp/"))
+  (normal-top-level-add-to-load-path '("."))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(let ((basedir "~/.emacs.d/themes/"))
+  (dolist (f (directory-files basedir))
+    (if (and (not (or (equal f ".") (equal f "..")))
+      (file-directory-p (concat basedir f)))
+    (add-to-list 'custom-theme-load-path (concat basedir f)))))
+
+(load-theme 'jbeans t)
+
+(setq backup-directory-alist `(("." . "~/.emacs.d/.saves")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (wakatime-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(global-wakatime-mode)
+
+(global-set-key [home]		'move-beginning-of-line)
+(global-set-key [end]		'move-end-of-line)
+(global-set-key [PageUp]	'scroll-down)
+(global-set-key [PageDown]	'scroll-up)
